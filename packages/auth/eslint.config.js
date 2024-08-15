d57e813c08bf65244bc3f9f21e26a589fed40c86
Turbobot <turbobot@vercel.com>
2024-08-15 00:38:47 -0300

    feat(create-turbo): create https://github.com/t3-oss/create-t3-turbo

---
+++ packages/auth/eslint.config.js
@@ -0,0 +1,10 @@
+import baseConfig, { restrictEnvAccess } from "@nutrilife/eslint-config/base";
+
+/** @type {import('typescript-eslint').Config} */
+export default [
+  {
+    ignores: [],
+  },
+  ...baseConfig,
+  ...restrictEnvAccess,
+];
---
+++ tooling/tailwind/eslint.config.js
@@ -0,0 +1,6 @@
+// FIXME: This kinda stinks...
+/// <reference types="../../tooling/eslint/types.d.ts" />
+
+import baseConfig from "@nutrilife/eslint-config/base";
+
+export default [...baseConfig];
