144ed711510ab7e2e51e5674d63c1b6d71f95c54
joaoprocopio <joaovitorcprocopio@gmail.com>
2024-08-17 00:46:47 -0300

    vai fastify

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -1,17 +1,50 @@
 import Fastify from "fastify"
-const fastify = Fastify({
-  logger: true
+
+const server = Fastify({
+  logger: {
+    transport: {
+      target: "pino-pretty",
+      options: {
+        translateTime: "HH:MM:ss Z",
+        ignore: "pid,hostname"
+      }
+    }
+  }
 })
 
-// Declare a route
-fastify.get("/", async function handler(request, reply) {
-  return { hello: "world" }
-})
+const settings = {
+  get PORT() {
+    return parseInt(process.env.PORT!) || 8000
+  },
+  get HOST() {
+    return process.env.HOST || "0.0.0.0"
+  },
 
-// Run the server!
-try {
-  fastify.listen({ port: 3000 })
-} catch (err) {
-  fastify.log.error(err)
-  process.exit(1)
+  get DB_NAME() {
+    return process.env.DB_NAME || "postgres"
+  },
+  get DB_USER() {
+    return process.env.DB_USER || "postgres"
+  },
+  get DB_PASSWORD() {
+    return process.env.DB_PASSWORD || "postgres"
+  },
+  get DB_HOST() {
+    return process.env.DB_HOST || "localhost"
+  },
+  get DB_PORT() {
+    return parseInt(process.env.DB_PORT!) || 5432
+  }
+} as const
+
+async function main() {
+  try {
+    await server.listen({ port: settings.PORT, host: settings.HOST })
+  } catch (error) {
+    server.log.error(error)
+
+    process.exit(1)
+  }
 }
+
+main()
