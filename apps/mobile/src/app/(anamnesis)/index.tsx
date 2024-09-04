b99b4be41800f2f57d2f51c17090e44973993e68
joaoprocopio <joaovitorcprocopio@gmail.com>
2024-09-03 21:19:36 -0300

    component lib

---
+++ apps/mobile/src/app/(anamnesis)/index.tsx
@@ -0,0 +1,10 @@
+import { Button, Text, View } from "tamagui"
+
+export default function Anamnesis() {
+  return (
+    <View>
+      <Text>Anamnesis</Text>
+      <Button>RAFAO MINECRAFT</Button>
+    </View>
+  )
+}
--- apps/mobile/src/app/_layout.tsx
+++ apps/mobile/src/app/_layout.tsx
@@ -1,10 +1,13 @@
 import "react-native-reanimated"
+import "../tamagui-web.css"
 
 import { DarkTheme, DefaultTheme, ThemeProvider } from "@react-navigation/native"
 import { Stack } from "expo-router"
 import * as SplashScreen from "expo-splash-screen"
+import { useColorScheme } from "react-native"
+import { TamaguiProvider } from "tamagui"
 
-import { useColorScheme } from "@/components/useColorScheme"
+import { tamaguiConfig } from "@@/tamagui.config"
 
 export {
   // Catch any errors thrown by the Layout component.
@@ -12,8 +15,7 @@ export {
 } from "expo-router"
 
 export const unstable_settings = {
-  // Ensure that reloading on `/modal` keeps a back button present.
-  initialRouteName: "(tabs)"
+  initialRouteName: "(anamnesis)"
 }
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
@@ -29,11 +31,12 @@ function RootLayoutNav() {
   const colorScheme = useColorScheme()
 
   return (
-    <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: "modal" }} />
-      </Stack>
-    </ThemeProvider>
+    <TamaguiProvider config={tamaguiConfig} defaultTheme={colorScheme!}>
+      <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
+        <Stack>
+          <Stack.Screen name="(anamnesis)" options={{ headerShown: false }} />
+        </Stack>
+      </ThemeProvider>
+    </TamaguiProvider>
   )
 }
