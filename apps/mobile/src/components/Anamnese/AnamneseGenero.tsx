b882e19ab111bcd7f26b22789f63834cad7ca87b
otavioabreu27 <otavio.abreu96@gmail.com>
2024-09-16 19:32:52 -0300

    adicionando componentes anamnese

---
+++ apps/mobile/src/components/Anamnese/AnamneseGenero.tsx
@@ -0,0 +1,62 @@
+import { StyleSheet, Text } from "react-native"
+
+import { StepIndicator } from "@/components/Anamnese/AnamneseStepIndicator"
+import { CardFlex } from "@/components/Anamnese/CardFlex"
+import { PaginationButtons } from "@/components/Anamnese/NextPrevButtons"
+import { View } from "@/components/Themed"
+
+export default function AnamneseGenero() {
+  return (
+    <View style={styles.containerPage}>
+      <StepIndicator totalSteps={8} currentStep={2} />
+      <View style={styles.containerContent}>
+        <Text style={styles.mainText}>Qual o seu sexo?</Text>
+        <View style={styles.containerButtons}>
+          <CardFlex selected={false} content="Masculino" />
+          <CardFlex selected={true} content="Feminino" />
+        </View>
+      </View>
+      <View style={styles.containerPagination}>
+        <PaginationButtons showNext={true} showPrevious={true} />
+      </View>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  containerPage: {
+    flex: 1,
+    backgroundColor: "#F4F4F4",
+    paddingHorizontal: 30,
+    paddingVertical: 15,
+    justifyContent: "space-between"
+  },
+  containerContent: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    width: "100%",
+    backgroundColor: "#F4F4F4",
+    gap: 20
+  },
+  containerButtons: {
+    flexShrink: 1, // Allow shrinking based on its content
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#F4F4F4",
+    width: "100%",
+    gap: 15
+  },
+  containerPagination: {
+    height: 100,
+    width: "100%",
+    justifyContent: "flex-end",
+    alignItems: "center",
+    marginBottom: 20
+  },
+  mainText: {
+    fontSize: 24,
+    textAlign: "left",
+    width: "100%"
+  }
+})
--- apps/mobile/src/components/EditScreenInfo.tsx
+++ apps/mobile/src/components/EditScreenInfo.tsx
@@ -30,13 +30,12 @@ export default function EditScreenInfo({ path }: { path: string }) {
           Change any of the text, save the file, and your app will automatically update.
         </Text>
       </View>
-
       <View style={styles.helpContainer}>
         <ExternalLink
           style={styles.helpLink}
           href="https://docs.expo.io/get-started/create-a-new-app/#opening-the-app-on-your-phonetablet">
           <Text style={styles.helpLinkText} lightColor={Colors.light.tint}>
-            Tap here if your app doesn't automatically update after making changes
+            Tap here if your app does not automatically update after making changes
           </Text>
         </ExternalLink>
       </View>
