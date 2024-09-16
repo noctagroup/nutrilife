9ea8618987dc47186a832ec661c203d8c5930718
biamontanini <montanini249@gmail.com>
2024-09-16 19:08:11 -0300

    Adicionando commit sem tabs

--- apps/mobile/src/app/(tabs)/index.tsx
+++
@@ -1,7 +0,0 @@
-import { StyleSheet, View } from "react-native"
-
-import FormularioLogin from "@/components/Login/FormularioLogin"
-
-export default function TabOneScreen() {
-  return <FormularioLogin />
-}
