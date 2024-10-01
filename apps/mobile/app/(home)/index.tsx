b171c9e1a81f28d38677e017b2bf88ff7df79750
Otávio Abreu dos Santos Silva <54289303+otavioabreu27@users.noreply.github.com>
2024-10-01 10:37:21 -0300

    15 integrao login (#16)
    * nao consegui programar estou ficando louco
    
    * adicionando a integracao do login e exemplo de autenticacao na home

--- apps/mobile/app/(home)/index.tsx
+++ apps/mobile/app/(home)/index.tsx
@@ -1,6 +1,15 @@
+import Authorization from "@/hooks/Authorization"
+import { useRouter } from "expo-router"
+import { useEffect } from "react"
 import { Text, View } from "react-native"
 
 export default function HomeIndex() {
+  const router = useRouter()
+
+  useEffect(() => {
+    Authorization.isAuthorized(router);
+  }, [])
+
   return (
     <View>
       <Text>vou ser a home com os dados de nutrição</Text>
